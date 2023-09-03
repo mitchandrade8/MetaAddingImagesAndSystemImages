fb3cd0e6c7bcc2bffa1d481eefb480d0ca1e76c6
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-03 15:51:32 -0600

    Initial Commit

---
+++ MetaAddingImagesAndSystemImages/MetaAddingImagesAndSystemImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaAddingImagesAndSystemImagesApp.swift
+//  MetaAddingImagesAndSystemImages
+//
+//  Created by Mitch Andrade on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaAddingImagesAndSystemImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
